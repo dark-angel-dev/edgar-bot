b6c01d08932ce5835864393b55024553b1044655
abdo <113805445+abdo0505@users.noreply.github.com>
2024-09-01 17:17:28 +0100

    Update downloader-apkx.js

--- plugins/downloader-apkx.js
+++ plugins/downloader-apkx.js
@@ -3,65 +3,104 @@ import fetch from 'node-fetch';
 const { generateWAMessageFromContent, proto, prepareWAMessageMedia } = pkg;
 
 let handler = async (m, { conn, args, text, usedPrefix, command }) => {
-    if (!args[0]) throw 'Ex: ' + usedPrefix + command + ' name app';
-    let q = args[0];
-    let apiUrl = `https://lovely-moral-asp.ngrok-free.app/api/apkpure?q=${q}`;
-    let response = await fetch(apiUrl);
-    if (!response.ok) throw 'Error fetching APK data';
-    let apkData = await response.json();
+    if (command === 'apk') {
+        if (!args[0]) throw 'Ex: ' + usedPrefix + command + ' Facebook lite';
+        await m.reply("*LOADING...*");
+        let q = text;
+        let apiUrl = `https://mee-api.vercel.app/api/apkpure?q=${q}`;
+        let response = await fetch(apiUrl);
+        if (!response.ok) throw 'Error fetching APK data';
 
-    const list = apkData.map((app, index) => {
-        let json = JSON.stringify({
-            downloadUrl: app.downloadUrl,
-            downloadType: app.downloadType,
-            packageName: app.packageName
+        let apkData = await response.json();
+        if (!apkData || apkData.length === 0) throw 'No APK data found';
+
+        const list = apkData.map((app, index) => {
+            let json = JSON.stringify({
+                downloadUrl: app.downloadUrl,
+                downloadType: app.downloadType,
+                packageName: app.packageName
+            });
+
+            return {
+                title: `App ${index + 1}: ${app.title}`,
+                rows: [
+                    {
+                        title: app.title,
+                        id: `${usedPrefix}doapk ${json}`
+                    }
+                ]
+            };
         });
 
-        return {
-            title: `App ${index + 1}: ${app.title}`,
-            rows: [
-                {
-                    title: app.title,
-                    id: `.doapk`
-                }
-            ]
+        const sections = list.map((item) => {
+            return {
+                title: item.title,
+                rows: item.rows
+            };
+        });
+
+        const buttonParamsJson = JSON.stringify({
+            title: "Available APKs",
+            sections: sections
+        });
+        
+        let icon = apkData[0].icon;
+        if (!icon) throw 'No icon found for the APK!';
+
+        const interactiveMessage = {
+            body: { text: "Choose an APK to download :" },
+            footer: { text: "_by JeenTeam_" },
+            header: {
+                hasMediaAttachment: true,
+                ...(await prepareWAMessageMedia({ image: { url: icon } }, { upload: conn.waUploadToServer }))
+            },
+            nativeFlowMessage: {
+                buttons: [{
+                    name: "single_select",
+                    buttonParamsJson
+                }]
+            }
         };
-    });
 
-    const sections = list.map((item) => {
-        return {
-            title: item.title,
-            rows: item.rows
+        const message = {
+            messageContextInfo: { deviceListMetadata: {}, deviceListMetadataVersion: 2 },
+            interactiveMessage
         };
-    });
 
-    const buttonParamsJson = JSON.stringify({
-        title: "Available APKs",
-        sections: sections
-    });
+        await conn.relayMessage(m.chat, { viewOnceMessage: { message } }, {});
+    } else if (command === 'doapk') {
+        if (!text) throw 'Error: No data provided.';
+        
+        const json = text;
+        const parsedData = JSON.parse(json);
+        
+        await m.reply("Please Wait 🫸🏻");
+        
+        const downloadUrl = parsedData.downloadUrl;
+        const packageName = parsedData.packageName;
+        let downloadType = parsedData.downloadType;
 
-    const interactiveMessage = {
-        body: { text: "Choose an APK to download :" },
-        footer: { text: "_by Mee6Team_" },
-        header: {
-            hasMediaAttachment: true,
-            ...(await prepareWAMessageMedia({ image: { url: apkData[0].icon } }, { upload: conn.waUploadToServer }))
-        },
-        nativeFlowMessage: {
-            buttons: [{
-                name: "single_select",
-                buttonParamsJson
-            }]
+        if (downloadType !== 'apk' && downloadType !== 'xapk') {
+            downloadType = 'apk';
         }
-    };
 
-    const message = {
-        messageContextInfo: { deviceListMetadata: {}, deviceListMetadataVersion: 2 },
-        interactiveMessage
-    };
+        let response = await fetch(downloadUrl, { method: 'HEAD' });
+        let mimetype = response.headers.get('content-type');
+        let size = response.headers.get('Content-Length');
 
-    await conn.relayMessage(m.chat, { viewOnceMessage: { message } }, {});
+        if (size > 699 * 1024 * 1024) throw 'File size exceeds 699 MB';
+
+        const fileName = `${packageName}.${downloadType}`;
+
+        await conn.sendMessage(
+            m.chat,
+            { document: { url: downloadUrl }, mimetype: mimetype, fileName: fileName },
+            { quoted: m }
+        );
+    }
 };
 
-handler.command = /^(apk)$/i;
+handler.command = /^(apk|doapk)$/i;
+handler.help = ['apk']
+handler.tags = ['downloader']
 export default handler;
