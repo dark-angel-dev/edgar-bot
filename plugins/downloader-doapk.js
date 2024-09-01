f1ccce7d17f38ea7bfe0652e0160f2384232042a
abdo <113805445+abdo0505@users.noreply.github.com>
2024-09-01 22:22:18 +0100

    Delete plugins directory

--- plugins/downloader-doapk.js
+++
@@ -1,6 +0,0 @@
-let handler = async (m, { conn, args, text, usedPrefix, command }) => {
-m.reply("hello");
-};
-
-handler.command = /^(doapk)$/i;
-export default handler;
--- plugins/owner-owner.js
+++
@@ -1,9 +0,0 @@
-function handler(m) {
-  const data = global.owner.filter(([id, isCreator]) => id && isCreator)
-  this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
-
-}
-
-handler.command = ['owner', 'creator', 'creador', 'dueño', 'Gowner'] 
-
-export default handler
